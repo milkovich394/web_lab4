46448b441adccc847c852957e5b351e02b448442
milkovich394 <111428437+milkovich394@users.noreply.github.com>
2024-03-20 18:51:24 +0500

    Add files via upload

---
+++ src/mockData/footerData.js
@@ -0,0 +1,91 @@
+export const headerOfFooter = {
+    header: "Хотите Шагнуть в Будущее Раньше Других?",
+    button:{
+        href: "#",
+        text: "Запросить ранний доступ",
+    },
+};
+
+export const infoGpt3 = {
+    img:{
+        src: "./assets/img/GPT-3.svg",
+        alt: "not-found",
+    },
+    text: `ул. Профессора Поздеева, 13, к.Г,
+    Пермь, Пермский край, 614013
+    Все права защищены`,
+};
+
+export const footerLinks = {
+        title: "Ссылки",
+        items: [
+            {
+            href: "#",
+            text: "Овероны",
+            },
+            {
+            href: "#",
+            text: "Соц. сети",
+            },
+            {
+            href: "#",
+            text: "Счетчики",
+            },
+            {
+            href: "#",
+            text: "Контакты",
+            },
+        ],
+    };
+
+export const companyLinks = 
+    {
+        title: "Компания",
+        items: [
+            {
+            href: "#",
+            text: "Условия использования",
+            },
+            {
+            href: "#",
+            text: "Перс. данные",
+            },
+            {
+            href: "#",
+            text: "Конаткты",
+            },
+        ],
+    };
+
+export const contactLinks = 
+    {
+        title: "Контакты",
+        items: [
+            {
+            href: "#",
+            text: "ул. Профессора Поздеева, 13, к.Г, Пермь",
+            },
+            {
+            href: "#",
+            text: "+7 (342) 2-198-520",
+            },
+            {
+            href: "#",
+            text: "info@pstu.ru",
+            },
+        ],
+    };
+
+export const theEnd ={
+    text: '© 2023 GPT-3. Все права защищены.',
+};
+
+export const footerData ={
+    headerOfFooter,
+    infoGpt3,
+    footerLinks,
+    companyLinks,
+    contactLinks,
+};
+
+export default footerData;
---
+++ src/mockData/registrationData.js
@@ -0,0 +1,10 @@
+const registrationData = {
+    title: "Запросите ранний доступ",
+    header: "Зарегистрируйтесь и начните исследовать безграничные возможности.",
+    button: {
+      href: "#",
+      text: "Начать",
+    },
+  };
+
+export default registrationData;
